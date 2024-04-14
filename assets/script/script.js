b6b7fc9647c5959e7dabf15e1bd013d26be8e6a1
HidanYugakure <fidants@code.edu.az>
2024-04-14 19:35:33 +0400

    add navbar

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,34 @@
-$(document).ready(function() {
-    
+// Sayfa yüklendiğinde çalışacak fonksiyon
+document.addEventListener('DOMContentLoaded', function() {
+    openTab('brands');
 
+
+    var initialTabButton = document.querySelector('.tablink[data-tab="brands"]');
+    if (initialTabButton) {
+        initialTabButton.classList.add('active');
+    }
 });
+
+
+function openTab(tabName) {
+  
+    var tabContents = document.querySelectorAll('.tabcontent');
+    tabContents.forEach(function(content) {
+        content.style.display = 'none';
+    });
+
+  
+    var tabLinks = document.querySelectorAll('.tablink');
+    tabLinks.forEach(function(link) {
+        link.classList.remove('active');
+    });
+
+
+    document.getElementById(tabName).style.display = 'block';
+
+  
+    var clickedTabButton = document.querySelector('.tablink[data-tab="' + tabName + '"]');
+    if (clickedTabButton) {
+        clickedTabButton.classList.add('active');
+    }
+}
